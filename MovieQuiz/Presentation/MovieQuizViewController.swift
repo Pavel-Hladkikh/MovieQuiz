2651909e2bf03d7fe3e0cf96466c3ef526f503b4
Pavel Hladkikh <pavel.hladkikh@yandex.com>
2025-04-25 16:07:22 +0200

    Добавил скругление изображения, защиту от повторных нажатий и почистил код

--- MovieQuiz/Presentation/MovieQuizViewController.swift
+++ MovieQuiz/Presentation/MovieQuizViewController.swift
@@ -1,24 +1,15 @@
 import UIKit
-
-
-// шрифты не отображаются в Attributes Inspector
 extension UIFont {
     static let ysMedium20 = UIFont(name: "YSDisplay-Medium", size: 20)
     static let ysBold23 = UIFont(name: "YSDisplay-Bold", size: 23)
 }
-
 final class MovieQuizViewController: UIViewController {
-    
-    
     @IBOutlet private var imageView: UIImageView!
     @IBOutlet private var textLabel: UILabel!
     @IBOutlet private var counterLabel: UILabel!
-    // Добавил questionTitleLabel, yesButton, noButton что бы прописать шрифты
     @IBOutlet private var questionTitleLabel: UILabel!
     @IBOutlet private var yesButton: UIButton!
     @IBOutlet private var noButton: UIButton!
-    
-    // массив вопросов
     private let questions: [QuizQuestion] = [
         QuizQuestion(image: "The Godfather", text: "Рейтинг этого фильма больше чем 6?", correctAnswer: true),
         QuizQuestion(image: "The Dark Knight", text: "Рейтинг этого фильма больше чем 6?", correctAnswer: true),
@@ -31,24 +22,19 @@ final class MovieQuizViewController: UIViewController {
         QuizQuestion(image: "Tesla", text: "Рейтинг этого фильма больше чем 6?", correctAnswer: false),
         QuizQuestion(image: "Vivarium", text: "Рейтинг этого фильма больше чем 6?", correctAnswer: false)
     ]
-    
     private var currentQuestionIndex = 0
     private var correctAnswers = 0
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        //Прописал шрифты
         textLabel.font = UIFont.ysBold23
         counterLabel.font = UIFont.ysMedium20
         yesButton.titleLabel?.font = UIFont.ysMedium20
         noButton.titleLabel?.font = UIFont.ysMedium20
         questionTitleLabel.font = UIFont.ysMedium20
-        // Добавил логику показа первого вопроса
         let currentQuestion = questions[currentQuestionIndex]
         let viewModel = convert(model: currentQuestion)
         show(quiz: viewModel)
     }
-    
     private func convert(model: QuizQuestion) -> QuizStepViewModel {
         let questionStep = QuizStepViewModel(
             image: UIImage(named: model.image) ?? UIImage(),
@@ -56,14 +42,16 @@ final class MovieQuizViewController: UIViewController {
             questionNumber: "\(currentQuestionIndex + 1)/\(questions.count)")
         return questionStep
     }
-    
     private func show(quiz step: QuizStepViewModel) {
         imageView.layer.borderWidth = 0
         imageView.image = step.image
         textLabel.text = step.question
         counterLabel.text = step.questionNumber
+        imageView.layer.cornerRadius = 20
+        imageView.layer.masksToBounds = true
+        yesButton.isEnabled = true
+        noButton.isEnabled = true
     }
-    
     private func showNextQuestionOrResults() {
         if currentQuestionIndex == questions.count - 1 {
             let resultMessage = "Ваш результат: \(correctAnswers)/10"
@@ -80,8 +68,9 @@ final class MovieQuizViewController: UIViewController {
             show(quiz: viewModel)
         }
     }
-    
     private func showAnswerResult(isCorrect: Bool) {
+        yesButton.isEnabled = false
+        noButton.isEnabled = false
         if isCorrect {
             correctAnswers += 1
         }
@@ -98,7 +87,6 @@ final class MovieQuizViewController: UIViewController {
             title: result.title,
             message: result.text,
             preferredStyle: .alert)
-        
         let action = UIAlertAction(title: result.buttonText, style: .default) { _ in
             self.currentQuestionIndex = 0
             self.correctAnswers = 0
@@ -109,37 +97,29 @@ final class MovieQuizViewController: UIViewController {
         alert.addAction(action)
         self.present(alert, animated: true, completion: nil)
     }
-    
     @IBAction private func yesButtonClicked(_ sender: UIButton) {
         let currentQuestion = questions[currentQuestionIndex]
         let givenAnswer = true
         showAnswerResult(isCorrect: givenAnswer == currentQuestion.correctAnswer)
     }
-    
     @IBAction private func noButtonClicked(_ sender: UIButton) {
         let currentQuestion = questions[currentQuestionIndex]
         let givenAnswer = false
         showAnswerResult(isCorrect: givenAnswer == currentQuestion.correctAnswer)
     }
-    
     struct QuizStepViewModel {
         let image: UIImage
         let question: String
         let questionNumber: String
     }
-    
     struct QuizResultsViewModel {
         let title: String
         let text: String
         let buttonText: String
     }
-    
     struct QuizQuestion {
         let image: String
         let text: String
         let correctAnswer: Bool
     }
-    
-    
-    
 }
